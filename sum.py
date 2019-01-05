d025fb914463216cbd4285a3d17de7cdf9e61030
Hernan Razo <hernanrazo@gmail.com>
2019-01-05 16:33:01 -0600

    added tokenization function

--- sum.py
+++ sum.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
 from nltk.probability import FreqDist
@@ -8,9 +9,47 @@ from collections import defaultdict
 
 #nltk.download('punkt')
 #nltk.download('stopwords')
-#nltk.download('vader_lexicon')
 
 article_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/article.txt')
 stop_words = nltk.corpus.stopwords.words('english')
+custom = ['?','(', ')', '.', '[', ']','!', '...',
+';',"`","'",'"',',','-','$', "'s", "'ll", 'ca', "n't", "'m", "'re", "'ve"]
+stop_words.extend(custom)
 
-print('it worked')
+
+def open_file(article_path):
+
+	try:
+		with open(article_path, 'r') as file:
+			return file.read()
+
+	except IOError as e:
+		print('Error trying to read file')
+
+
+def clean_data():
+
+	filtered_words = []
+	filtered_sent = []
+
+	data = open_file(article_path)
+	data = data.lower()
+
+	tokenized_words = nltk.word_tokenize(data)
+	tokenized_sent = nltk.sent_tokenize(data)
+	tokenized_words = [words for words in tokenized_words if not words in stop_words]
+
+	for words in tokenized_words:
+		if words not in stop_words:
+			filtered_words.append(words)
+
+
+	for words in tokenized_sent:
+		if words not in stop_words:
+			filtered_sent.append(words)
+
+	return(filtered_words)
+	return(filtered_sent)
+
+
+#clean_data()
