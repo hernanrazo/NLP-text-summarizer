f1d756c630ed6d4aa36f47aeaa1938639144507e
Hernan Razo <hernanrazo@gmail.com>
2019-01-07 20:54:24 -0600

    finished but algorithm needs improving

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple Text Summarizer
+===
+
+work in progress
+
+Sources and Helpful Links
+---
+
+[Wisniewski, Mary. “CTA Completes 95th Street Red Line Station - Most Costly in Agency History.” Chicago Tribune, Chicago Tribune, 4 Jan. 2019, www.chicagotribune.com/news/local/breaking/ct-biz-cta-95th-street-station-20190103-story.html.](https://www.chicagotribune.com/news/local/breaking/ct-biz-cta-95th-street-station-20190103-story.html)  
+[McKinley, William. “A Nation Apart [Possibly The Longest Article Posted].” Freerepublic.com, The Economist, 18 Nov. 2003, www.freerepublic.com/focus/f-news/1023979/posts.](http://www.freerepublic.com/focus/f-news/1023979/posts)  
--- sum.py
+++ sum.py
@@ -9,39 +9,62 @@ from collections import defaultdict
 #nltk.download('punkt')
 #nltk.download('stopwords')
 
+#set local paths for txt files containing the article and another one for the 
+#created file where the summary will be printed onto
 article_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/article.txt')
+summary_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/sum.txt')
+
+#set stopwords with a few customs
 stop_words = nltk.corpus.stopwords.words('english')
 custom = ['?','(', ')', '.', '[', ']','!' ,
 ';',"`","'",'"',',','-',':','’', '$', "'s", "'ll", 'ca', "n't", "'m", "'re", "'ve"]
 stop_words.extend(custom)
 
-length = 5
+#set number of sentences for the summary
+#Toggle this if you wish
+length = 4
 
+#set function for tokenization of words
 def get_word_tokens():
 
+	#open and read article 
 	data = open(article_path, 'r').read()
+	
+	#set all letters to lowercase
 	data = data.lower()
 
+	#tokenize by individual words
 	tokenized_words = word_tokenize(data)
+
+	#filter out stopwords
 	tokenized_words = [word for word in tokenized_words if word not in stop_words]
 
 	return tokenized_words
 
+#set function to filter 
 def get_sent_tokens():
-
+	
+	#open and read article
 	data = open(article_path, 'r').read()
+	
+	#set all letters to lowercase
 	data = data.lower()
 
+	#tokenize by sentences
 	tokenized_sent = sent_tokenize(data)
+
+	#filter out stopwords
 	tokenized_sent = [word for word in tokenized_sent if word not in stop_words]
 
 	return tokenized_sent
 
-
+#calculate frequency distribution
 def get_freq_dist(words):
 
 	freq_dist = dict()
 
+	#calculate the frequency of occurance for each word
+	#incrementing its frequency by one everytime it occurs
 	for word in words:
 		if word in freq_dist:
 			freq_dist[word] += 1
@@ -50,34 +73,58 @@ def get_freq_dist(words):
 
 	return freq_dist
 
+#calculate the scores of each sentence
 def get_score(words, sentences, freq_dist):
 
 	score = defaultdict(int)
 
+	#iterate through each unique sentence
 	for counter, sentence in enumerate(sentences):
+
+		#iterate through each word
 		for word in words:
+
+			#if the word is in the frequency distribution
 			if word in freq_dist:
+
+				#add each word's frequency value to the 
+				#sentence's total score
 				score[counter] += freq_dist[word]
 
 	return score
 
+#set function to decide which sentences to include in
+#the summary
 def get_summary(score, sentences, length):
 
+	#create an index of scores
 	index = nlargest(length, score, key=score.get)
+	
+	#rearrange the sentence in sorted order
 	summary = [sentences[i] for i in sorted(index)]
+	
+	#join these sentences together into a shorter
+	#version of the original article
 	result = ' '.join(summary)
 
 	return result
+	return index
 
-
+#call tokenization functions for words and sentences
 words = get_word_tokens()
 sentences = get_sent_tokens()
 
+#call functions to get the frequency distribution and 
+#the score for each sentence
 freq_dist = get_freq_dist(words)
-
 score = get_score(words, sentences, freq_dist)
 
+#get the summary using the previously calculated values
 summary = get_summary(score, sentences, length)
-#clean this and put in a new txt file??
-print(summary)
 
+#write the resulting summary in a new txt file
+#and save it locally
+result = open(summary_path, 'w')
+result.write(summary)
+result.close()
+print('done')
