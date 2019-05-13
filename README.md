83854b13dc742d5ebc6f72afe55dd1c6a39ecbfc
Hernan Razo <hrazo@hawk.iit.edu>
2019-05-12 19:03:18 -0500

    started over

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ work in progress
 
 Sources and Helpful Links
 ---
+https://towardsdatascience.com/write-a-simple-summarizer-in-python-e9ca6138a08e?gi=d2fd75c14a95
+https://stackabuse.com/text-summarization-with-nltk-in-python/
+
 
 [Wisniewski, Mary. “CTA Completes 95th Street Red Line Station - Most Costly in Agency History.” Chicago Tribune, Chicago Tribune, 4 Jan. 2019, www.chicagotribune.com/news/local/breaking/ct-biz-cta-95th-street-station-20190103-story.html.](https://www.chicagotribune.com/news/local/breaking/ct-biz-cta-95th-street-station-20190103-story.html)  
 [McKinley, William. “A Nation Apart [Possibly The Longest Article Posted].” Freerepublic.com, The Economist, 18 Nov. 2003, www.freerepublic.com/focus/f-news/1023979/posts.](http://www.freerepublic.com/focus/f-news/1023979/posts)  
--- script.py
+++ script.py
@@ -1,66 +1,11 @@
-import spacy 
-from spacy.lang.en.stop_words import STOP_WORDS
 
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
+#set local paths for txt files containing the article and another one for the 
+#created file where the summary will be printed onto
 article_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/article.txt')
 summary_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/sum.txt')
-
-nlp = spacy.load('en_core_web_sm')
-
-nlp.Defaults.stop_words |= {'?','(', ')', '.', '[', ']','!', 'th',';',"`","'",'"',',','$', '/'}
-
-length = 4
-
-
-def get_word_tokens(path):
-    
-    filtered_words = []
-
-    file = open(path, 'r')
-    data = file.read()
-    file.close()
-
-    data = data.lower()
-    datta = data.replace('\n', ' ')
-
-    doc = nlp(data)
-
-    for word in doc:
-        if word.is_stop == False:
-            filtered_words.append(word)
-
-    return filtered_words
-
-    
-def get_sent_tokens(path):
-
-    filtered_sent = []
-    
-    words = get_word_tokens(path)
-
-    sentences = (''.join(str(words)))
-
-    for sentence in sentences:
-        filtered_sent.append(sentence)
-
-    return filtered_sent
-
-
-#sent = ('%s' % ''.join(map(str, sentences)))
-
-#sent = (''.join(list(chain.from_iterable(sentences))))
-
-    
-
-
-
-#words = get_word_tokens(article_path)
-#print(words)
-
-sentences = get_sent_tokens(article_path)
-
-print(sentences)
-
 
 
 
