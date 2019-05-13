1f4bd71d7c34e7d83ac643a682a6bee49a56a49c
Hernan Razo <hrazo@hawk.iit.edu>
2019-05-12 20:48:20 -0500

    shortened file path things. added get_article funciton that simply reads the article and prints it to terminal

--- script.py
+++ script.py
@@ -1,11 +1,29 @@
+import os
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 #nltk.download('punkt')
 #nltk.download('stopwords')
 
-#set local paths for txt files containing the article and another one for the 
-#created file where the summary will be printed onto
-article_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/article.txt')
-summary_path = ('/Users/hernanrazo/pythonProjects/NLP_summarizer/sum.txt')
+#set local paths for txt files containing the article and 
+#another one for the created file where the summary will be printed onto
+article_path = os.getcwd() + '/articles/article.txt'
+summary_path = os.getcwd() + 'summaries/sum.txt'
+
+#set stopwords
+stop_words = nltk.corpus.stopwords.words('english')
 
 
+def get_article(article):
+
+    text = open(article).read().lower()
+
+    print(text)
+
+
+
+
+get_article(article_path)
 
