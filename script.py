d9afd1c7bae54294027245357aee88d999e85614
Hernan Razo <hrazo@hawk.iit.edu>
2019-07-13 18:49:06 -0500

    added sentence scoring function

--- script.py
+++ script.py
@@ -31,15 +31,8 @@ def get_article(article):
 
     return text
 
-def intersection(sent1, sent2):
 
-    intersection = [i for i in sent1 if i in sent2]
-    normalize = len(intersection) / ((len(sent1) + len(sent2)) / 2)
-
-    return normalize
-
-
-def split_sentences(text):
+def split_sentences(sents):
 
     sentence_stream = [[i for i in word_tokenize(sent) if i not in stop_words] for sent in text]
 
@@ -50,33 +43,52 @@ def split_sentences(text):
     trigram = Phrases(bigram_tokens, min_count = 2, threshold = 2, delimiter = b'_')
     trigram_phraser = Phraser(trigram)
     trigram_tokens = trigram_phraser[bigram_tokens]
-    result = [i for i in trigram_tokens]
 
-    return result
+    all_words = [i for j in trigram_tokens for i in j]
+    freq_words = [i for i in Counter(all_words).most_common() if i[1] > 1]
+    sentences = [i for i in trigram_tokens]
 
+    return freq_words, sentences
+
+
+
+def score_sentences(words, sentences):
+
+    scores = Counter()
+
+    for word in words:
+        for i in range(0, len(sentences)):
+            if word[0] in sentences[i]: 
+                scores[i] += word[1]
+
+    sentences_scores = sorted(scores.items(), key = scores.__getitem__, reverse = True)
+
+    return sentence_scores
 
 
 def get_summary(text, limit = 3):
 
+    tokens = sent_tokenize(text)
     sentences = split_sentences(text)
-    matrix = [[intersection(sentences[i], sentences[j]) for i in range(0, len(sentences))] for j in range(0, len(sentences))]
+    freq_words = split_sentences(text)
+    sentence_scores = score_sentences(freq_words, sentences)
 
-    scores = {text[i]: sum(matrix[i]) for i in range(len(matrix))}
-    sentences = sorted(scores, key = scores.__getitem__, reverse = True)[:limit]
+    scores = {sents[i]: sum(matrix[i]) for i in range(len(matrix))}
+    sents = sorted(scores, key = scores.__getitem__, reverse = True)[:limit]
     best_sents = [i[0] for i in sorted([(i, text.find(i)) for i in sentences], key = lambda x: x[0])]
 
     return best_sents
 
 
+def summarize(text, limit = 3):
+
+    summary = get_summary(text, limit)
+    print(' '.join(summary))
+
+
+
+
+
 
 text = get_article(article_path)
-
-split = split_sentences(text) #shit isnt splitting right
-print(split)
-'''
-summary = get_summary(text)
-
-print(' '.join(summary))
-
-
-'''
+summarize(text, 5)
